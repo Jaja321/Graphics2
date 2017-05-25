32920a1605ce79e3454add33fd86de7c408bc4ee
Benjamin Mizrahi <benjajamin@gmail.com>
2017-05-25 17:23:56 +0300

    Added getPixelPosition method to Camera

--- src/Camera.java
+++ src/Camera.java
@@ -1,45 +1,65 @@
 
 public class Camera {
-	Vector position, direction, up;
+	Vector position, direction, up, right, screenOrigin;
 	float screenDistance, screenWidth, screenHeight;
-	
-	public Camera(Vector position, Vector lookat, Vector up, float screenDistance, float screenWidth, float screenHeight) {
+
+	public Camera(Vector position, Vector lookat, Vector up, float screenDistance, float screenWidth,
+			float screenHeight) {
 		this.position = position;
-		this.direction= Vector.subtract(lookat, position);
-		//Fixing up vector:
-		Vector temp=Vector.cross(direction, up);
-		temp=Vector.cross(direction, temp);
-		temp=temp.multiply(-1);
+		Vector dir = Vector.subtract(lookat, position);
+		dir = dir.divide(dir.norm());
+		this.direction = dir;
+		// Fixing up vector:
+		Vector temp = Vector.cross(direction, up);
+		temp = Vector.cross(direction, temp);
+		temp = temp.multiply(-1);
 		this.up = temp.divide(temp.norm());
-		
+
 		this.screenDistance = screenDistance;
 		this.screenWidth = screenWidth;
 		this.screenHeight = screenHeight;
+
+		// Calculate the bottom-left point of the screen:
+		right = Vector.cross(this.up, this.direction);
+		Vector left = right.multiply(-1);
+		Vector down = this.up.multiply(-1);
+		screenOrigin = Vector.add(position, direction.multiply(this.screenDistance));
+		screenOrigin = Vector.add(screenOrigin, left.multiply(this.screenWidth / 2));
+		screenOrigin = Vector.add(screenOrigin, down.multiply(this.screenHeight / 2));
+
 	}
-	
-	public static Camera parseCamera(String[] params, float aspectRatio){
+
+	public static Camera parseCamera(String[] params, float aspectRatio) {
 		float posX = Float.parseFloat(params[0]);
 		float posY = Float.parseFloat(params[1]);
 		float posZ = Float.parseFloat(params[2]);
 		Vector position = new Vector(posX, posY, posZ);
-		
+
 		float lookAtX = Float.parseFloat(params[3]);
 		float lookAtY = Float.parseFloat(params[4]);
 		float lookAtZ = Float.parseFloat(params[5]);
 		Vector lookAt = new Vector(lookAtX, lookAtY, lookAtZ);
-		
+
 		float upVectorX = Float.parseFloat(params[6]);
 		float upVectorY = Float.parseFloat(params[7]);
 		float upVectorZ = Float.parseFloat(params[8]);
 		Vector upVector = new Vector(upVectorX, upVectorY, upVectorZ);
-		
+
 		float distance = Float.parseFloat(params[9]);
 		float width = Float.parseFloat(params[10]);
-		float height = width/aspectRatio;
-		
+		float height = width / aspectRatio;
+
 		return new Camera(position, lookAt, upVector, distance, width, height);
 	}
-	
-	
-	
+
+	public Vector getPixelPosition(int x, int y, int imageWidth, int imageHeight) {
+		float pixelHeight = screenHeight / imageHeight;
+		float pixelWidth = screenWidth / imageWidth;
+		Vector tempUp = up.multiply(pixelHeight * y + 0.5f * pixelHeight);
+		Vector tempRight = right.multiply(pixelWidth * x + 0.5f * pixelWidth);
+		Vector position = Vector.add(screenOrigin, tempUp);
+		position = Vector.add(position, tempRight);
+		return position;
+	}
+
 }
