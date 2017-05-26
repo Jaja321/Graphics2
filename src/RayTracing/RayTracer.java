9974df1efd1ce4f8b4b845f18fd3448be03ab03a
talkami <talkaminker@yahoo.com>
2017-05-27 00:23:17 +0300

    getRayColor function moved to Scene class

--- src/RayTracing/RayTracer.java
+++ src/RayTracing/RayTracer.java
@@ -53,72 +53,14 @@ public class RayTracer {
 			// Render scene:
 			tracer.renderScene(outputFileName);
 
-			// } catch (IOException e) {
-			// System.out.println(e.getMessage());
 		} catch (RayTracerException e) {
 			System.out.println(e.getMessage());
+		} catch (IOException e) {
+		 System.out.println(e.getMessage());
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
-	}
-
-	public Color getRayColor(Ray ray) {
-		boolean noIntersect = true;
-		for (Surface surface : this.scene.getSurfaces()) {
-			if (surface.RayIntersect(ray))
-				noIntersect = false;
-		}
-		if (noIntersect)
-			return this.scene.getBackground();
-		Surface surface = ray.getClosestObject();
-		Material material = surface.getMaterial();
-		Color totalLight = new Color(0, 0, 0);
-
-		for (Light light : this.scene.getLights()) {
-			Color lightColor = new Color(0, 0, 0);
-
-			// Calculate diffuse component:
-			Vector lightDirection = Vector.subtract(light.position, ray.getRayVector());
-			float lightDistance = lightDirection.norm();
-			lightDirection = lightDirection.divide(lightDistance);
-			Color lightDiffuse = material.getDiffuse().multiplyColor(light.color);
-			float cos = Math.abs(Vector.dot(lightDirection, ray.getIntersectionNormal()));
-			lightDiffuse = lightDiffuse.multiplyColor(cos);
-			lightColor = lightColor.addColor(lightDiffuse);
-
-			// Calculate specular component:
-			float temp = Vector.dot(lightDirection, ray.getIntersectionNormal());
-			Vector lightReflection = ray.getIntersectionNormal().multiply(temp);
-			lightReflection = lightReflection.multiply(2);
-			lightReflection = Vector.subtract(lightReflection, lightDirection);
-			Vector viewDirection = ray.getDir().multiply(-1);
-			cos = Math.abs(Vector.dot(lightReflection, viewDirection));
-			Color lightSpecular = material.getSpecular().multiplyColor(light.color);
-			lightSpecular = lightSpecular.multiplyColor((float) Math.pow(cos, material.getPhong()));
-			lightSpecular = lightSpecular.multiplyColor(light.specular);
-			lightColor = lightColor.addColor(lightSpecular);
-
-			// Check if light is obstructed and add shadows:
-			Ray lightRay = new Ray(ray.getRayVector(), lightDirection);
-			boolean intersect = false;
-			for (Surface surf : this.scene.getSurfaces()) {
-				if (surf.RayIntersect(lightRay)) {
-					float intersectDistance = (float) lightRay.getT();
-					if (intersectDistance < lightDistance) {
-						intersect = true;
-						break;
-					}
-				}
-			}
-			if (intersect)
-				lightColor = lightColor.multiplyColor(1 - light.shadow);
-
-			totalLight = totalLight.addColor(lightColor);
-		}
-		// totalDiffuse=multiplyColors(totalDiffuse, material.getDiffuse());
-		// return material.getDiffuse();
-		return totalLight;
 	}
 
 	/**
@@ -210,17 +152,8 @@ public class RayTracer {
 
 		// Create a byte array to hold the pixel data:
 		byte[] rgbData = new byte[this.imageWidth * this.imageHeight * 3];
-		// Put your ray tracing code here!
-		//
-		// Write pixel color values in RGB format to rgbData:
-		// Pixel [x, y] red component is in rgbData[(y * this.imageWidth + x) *
-		// 3]
-		// green component is in rgbData[(y * this.imageWidth + x) * 3 + 1]
-		// blue component is in rgbData[(y * this.imageWidth + x) * 3 + 2]
-		//
-		// Each of the red, green and blue components should be a byte, i.e.
-		// 0-255
-		Vector cameraPos = this.scene.getCamera().position;
+
+		Vector cameraPos = this.scene.getCamera().getPosition();
 		for (int x = 0; x < this.imageWidth; x++) {
 			for (int y = 0; y < this.imageHeight; y++) {
 				Vector pixelPos = this.scene.getCamera().getPixelPosition(x, y, this.imageWidth, this.imageHeight);
@@ -228,7 +161,7 @@ public class RayTracer {
 				rayDir = rayDir.divide(rayDir.norm());
 				Ray ray = new Ray(cameraPos, rayDir);
 
-				Color color = getRayColor(ray);
+				Color color = this.scene.getColor(ray);
 
 				rgbData[(y * this.imageWidth + x) * 3] = color.getRedByte();
 				rgbData[(y * this.imageWidth + x) * 3 + 1] = color.getGreenByte();
@@ -252,6 +185,7 @@ public class RayTracer {
 
 	}
 
+	
 	//////////////////////// FUNCTIONS TO SAVE IMAGES IN PNG FORMAT
 	//////////////////////// //////////////////////////////////////////
 
