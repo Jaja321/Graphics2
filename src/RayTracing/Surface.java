679a8295c508b63827569ce4d7f0720ef4a07e73
talkami <talkaminker@yahoo.com>
2017-05-27 00:23:36 +0300

    added getters to all Color components from Material

--- src/RayTracing/Surface.java
+++ src/RayTracing/Surface.java
@@ -20,14 +20,34 @@ public abstract class Surface {
 	public Material getMaterial() {
 		return material;
 	}
-
 	public void setMaterial(Material material) {
 		this.material = material;
 	}
 	
-	public Color getDiffuseColor(){
+	public Color getDiffuse() {
 		return this.material.getDiffuse();
 	}
-	public abstract boolean RayIntersect(Ray ray);
+	public Color getSpecular() {
+		return this.material.getSpecular();
+	}
+	public Color getReflection() {
+		return this.material.getReflection();
+	}
+	public float getPhong() {
+		return this.material.getPhong();
+	}
+	public float getTransparency() {
+		return this.material.getTransparency();
+	}
+	
+	public Vector getReflectionVector(Vector light, Vector normal) { 
+		Vector light2 = light.multiply(2);
+		Vector LN2 = Vector.multiply(light2,normal);
+		Vector multi = Vector.multiply(LN2, normal);
+		Vector R = Vector.subtract(multi, light);
+		return R; // R = (2L*N)N - L
+	}
+
+	public abstract RayHit RayIntersect(Ray ray);
 	
 }
